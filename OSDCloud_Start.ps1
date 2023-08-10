b3140e03ef1cae9e4f4a42944122b95eeedda826
Gregory B <gregory.bouchu@outlook.com>
2023-08-10 13:12:18 +0200

    Update OSDCloud_Start.ps1

--- OSDCloud_Start.ps1
+++ OSDCloud_Start.ps1
@@ -12,8 +12,6 @@ Import-Module OSD -Force
 Write-Host -ForegroundColor Green "Start OSDPad"
 #Start-OSDPad -RepoOwner GregoryB74 -RepoName OSD-Cloud -RepoFolder ScriptPad -BrandingTitle 'Gregory B OSD Cloud Deployment'
 
-
-
 #================================================
 #   [PreOS] Update Module
 #================================================
@@ -31,22 +29,14 @@ Import-Module OSD -Force
 #=======================================================================
 #   [OS] Params and Start-OSDCloud
 #=======================================================================
-
-Write-Host -ForegroundColor Cyan "Set the Global Variables for a Driver Pack name --> none"
-$Global:MyOSDCloud = @{
-    DriverPackName = 'none'
-    #ApplyManufacturerDrivers = $false
-    #ApplyCatalogDrivers = $false
-    #ApplyCatalogFirmware = $false
-}
-
 $Params = @{
     OSVersion = "Windows 10"
     OSBuild = "22H2"
     OSEdition = "Enterprise"
     OSLanguage = "en-us"
-    Firmware = $false
+    OSLicense = "Volume"
     ZTI = $true
+    Firmware = $false
 }
 Start-OSDCloud @Params
 
@@ -56,34 +46,40 @@ Start-OSDCloud @Params
 Write-Host -ForegroundColor Green "Create C:\ProgramData\OSDeploy\OSDeploy.OOBEDeploy.json"
 $OOBEDeployJson = @'
 {
+    "AddNetFX3":  {
+                      "IsPresent":  true
+                  },
     "Autopilot":  {
                       "IsPresent":  false
                   },
     "RemoveAppx":  [
-                       "Microsoft.549981C3F5F10",
-                        "Microsoft.BingWeather",
-                        "Microsoft.GetHelp",
-                        "Microsoft.Getstarted",
-                        "Microsoft.Microsoft3DViewer",
-                        "Microsoft.MicrosoftOfficeHub",
-                        "Microsoft.MicrosoftSolitaireCollection",
-                        "Microsoft.MixedReality.Portal",
-                        "Microsoft.People",
-                        "Microsoft.SkypeApp",
-                        "Microsoft.Wallet",
-                        "Microsoft.WindowsCamera",
-                        "microsoft.windowscommunicationsapps",
-                        "Microsoft.WindowsFeedbackHub",
-                        "Microsoft.WindowsMaps",
-                        "Microsoft.Xbox.TCUI",
-                        "Microsoft.XboxApp",
-                        "Microsoft.XboxGameOverlay",
-                        "Microsoft.XboxGamingOverlay",
-                        "Microsoft.XboxIdentityProvider",
-                        "Microsoft.XboxSpeechToTextOverlay",
-                        "Microsoft.YourPhone",
-                        "Microsoft.ZuneMusic",
-                        "Microsoft.ZuneVideo"
+                    "MicrosoftTeams",
+                    "Microsoft.BingWeather",
+                    "Microsoft.BingNews",
+                    "Microsoft.GamingApp",
+                    "Microsoft.GetHelp",
+                    "Microsoft.Getstarted",
+                    "Microsoft.Messaging",
+                    "Microsoft.MicrosoftOfficeHub",
+                    "Microsoft.MicrosoftSolitaireCollection",
+                    "Microsoft.MicrosoftStickyNotes",
+                    "Microsoft.MSPaint",
+                    "Microsoft.People",
+                    "Microsoft.PowerAutomateDesktop",
+                    "Microsoft.StorePurchaseApp",
+                    "Microsoft.Todos",
+                    "microsoft.windowscommunicationsapps",
+                    "Microsoft.WindowsFeedbackHub",
+                    "Microsoft.WindowsMaps",
+                    "Microsoft.WindowsSoundRecorder",
+                    "Microsoft.Xbox.TCUI",
+                    "Microsoft.XboxGameOverlay",
+                    "Microsoft.XboxGamingOverlay",
+                    "Microsoft.XboxIdentityProvider",
+                    "Microsoft.XboxSpeechToTextOverlay",
+                    "Microsoft.YourPhone",
+                    "Microsoft.ZuneMusic",
+                    "Microsoft.ZuneVideo"
                    ],
     "UpdateDrivers":  {
                           "IsPresent":  true
@@ -101,20 +97,25 @@ $OOBEDeployJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.OOBEDeplo
 #================================================
 #  [PostOS] AutopilotOOBE Configuration Staging
 #================================================
+#Write-Host -ForegroundColor Green "Define Computername:"
+#$Serial = Get-WmiObject Win32_bios | Select-Object -ExpandProperty SerialNumber
+#$TargetComputername = $Serial.Substring(4,3)
+
+#$AssignedComputerName = "AkosCloud-$TargetComputername"
+#Write-Host -ForegroundColor Red $AssignedComputerName
+#Write-Host ""
+
 #Write-Host -ForegroundColor Green "Create C:\ProgramData\OSDeploy\OSDeploy.AutopilotOOBE.json"
-#$AutopilotOOBEJson = @'
+#$AutopilotOOBEJson = @"
 #{
+#    "AssignedComputerName" : "$AssignedComputerName",
+#    "AddToGroup":  "AADGroupX",
 #    "Assign":  {
 #                   "IsPresent":  true
 #               },
 #    "GroupTag":  "GroupTagXXX",
-#    "AddToGroup": "AADGroupX",
-#    "AddToGroupOptions":  [
-#                    "AADGroupX",
-#                    "AADroupY"
-#    ],
 #    "Hidden":  [
-#                   "AssignedComputerName",
+#                   "AddToGroup",
 #                   "AssignedUser",
 #                   "PostAction",
 #                   "GroupTag",
@@ -123,9 +124,10 @@ $OOBEDeployJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.OOBEDeplo
 #    "PostAction":  "Quit",
 #    "Run":  "NetworkingWireless",
 #    "Docs":  "https://google.com/",
-#    "Title":  "AkosCloud Autopilot Register"
+#    "Title":  "Autopilot Manual Register"
 #}
-#'@
+#"@
+#
 #If (!(Test-Path "C:\ProgramData\OSDeploy")) {
 #    New-Item "C:\ProgramData\OSDeploy" -ItemType Directory -Force | Out-Null
 #}
@@ -134,50 +136,40 @@ $OOBEDeployJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.OOBEDeplo
 #================================================
 #  [PostOS] AutopilotOOBE CMD Command Line
 #================================================
-Write-Host -ForegroundColor Green "Create C:\Windows\System32\OOBE-Test.cmd"
+Write-Host -ForegroundColor Green "Create C:\Windows\System32\OOBE.cmd"
 $OOBECMD = @'
 PowerShell -NoL -Com Set-ExecutionPolicy RemoteSigned -Force
 Set Path = %PATH%;C:\Program Files\WindowsPowerShell\Scripts
 Start /Wait PowerShell -NoL -C Install-Module AutopilotOOBE -Force -Verbose
 Start /Wait PowerShell -NoL -C Install-Module OSD -Force -Verbose
-Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://check-autopilotprereq.osdcloud.ch
-Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://start-autopilotoobe.osdcloud.ch
+Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/GregoryB74/OSD-Cloud/main/AP-Prereq.ps1
+Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/GregoryB74/OSD-Cloud/main/Start-AutopilotOOBE.ps1
 Start /Wait PowerShell -NoL -C Start-OOBEDeploy
-Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://tpm.osdcloud.ch
-Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://cleanup.osdcloud.ch
+Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/GregoryB74/OSD-Cloud/main/TPM.ps1
+Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/GregoryB74/OSD-Cloud/main/CleanUp.ps1
 Start /Wait PowerShell -NoL -C Restart-Computer -Force
 '@
-$OOBECMD | Out-File -FilePath 'C:\Windows\System32\OOBE-Test.cmd' -Encoding ascii -Force
+$OOBECMD | Out-File -FilePath 'C:\Windows\System32\OOBE.cmd' -Encoding ascii -Force
 
-# Commented
-#Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/AkosBakos/OSDCloud/main/Set-KeyboardLanguage.ps1
-#Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/AkosBakos/OSDCloud/main/Install-EmbeddedProductKey.ps1
+# Options:
+# Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/AkosBakos/OSDCloud/main/Set-KeyboardLanguage.ps1
+# Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/AkosBakos/OSDCloud/main/Install-EmbeddedProductKey.ps1
+# Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/GregoryB74/OSD-Cloud/main/Lenovo_BIOS_Settings.ps1
+
 
 #================================================
 #  [PostOS] SetupComplete CMD Command Line
 #================================================
 Write-Host -ForegroundColor Green "Create C:\Windows\Setup\Scripts\SetupComplete.cmd"
 $SetupCompleteCMD = @'
+powershell.exe -Command Set-ExecutionPolicy RemoteSigned -Force
+powershell.exe -Command "& {IEX (IRM https://raw.githubusercontent.com/GregoryB74/OSD-Cloud/main/oobetasks.ps1)}"
 '@
 $SetupCompleteCMD | Out-File -FilePath 'C:\Windows\Setup\Scripts\SetupComplete.cmd' -Encoding ascii -Force
-
-#================================================
-#  [PostOS] Installing August CU for Autopilot HW hash issues
-#================================================
-If ($Params.OSVersion -eq "Windows 10" -and $Params.OSBuild -eq "21H2") {
-    Write-Host -ForegroundColor Cyan "Installing CU for Autopilot HW hash issues"
-    Invoke-Expression (Invoke-RestMethod https://cu.osdcloud.ch)
-
-    Write-Host -ForegroundColor Gray "Download August CU PPKG from Azure Blob Storage"
-    Invoke-Expression "& curl.exe --insecure --location --output 'C:\OSDCloud\Packages\Install_CU.ppkg' --url 'https://XXXX.blob.core.windows.net/packages/Install_CU.ppkg'"
-    
-    Write-Host -ForegroundColor Gray "Importing August CU as PPKG"
-    DISM.exe /Image:C:\ /Add-ProvisioningPackage /PackagePath:C:\OSDCloud\Packages\Install_CU.ppkg
-}
 
 #=======================================================================
 #   Restart-Computer
 #=======================================================================
-Write-Host "Restarting in 20 seconds!" -ForegroundColor Green
+Write-Host  -ForegroundColor Green "Restarting in 20 seconds!"
 Start-Sleep -Seconds 20
 wpeutil reboot
