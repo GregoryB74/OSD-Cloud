f10e52092700508a356037f1290f1d7771697f0e
Gregory B <gregory.bouchu@outlook.com>
2023-08-18 14:22:28 +0200

    Ok

---
+++ Set-OOBETasksV2.ps1
@@ -0,0 +1,121 @@
+# OOBEtasks
+
+$scriptFolderPath = "$env:SystemDrive\OSDCloud\Scripts"
+$ScriptPathOOBE = $(Join-Path -Path $scriptFolderPath -ChildPath "OOBE.ps1")
+$ScriptPathSendKeys = $(Join-Path -Path $scriptFolderPath -ChildPath "SendKeys.ps1")
+
+If(!(Test-Path -Path $scriptFolderPath)) {
+    New-Item -Path $scriptFolderPath -ItemType Directory -Force | Out-Null
+}
+
+$OOBEScript =@"
+`$Global:Transcript = "`$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-OOBEScripts.log"
+Start-Transcript -Path (Join-Path "`$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" `$Global:Transcript) -ErrorAction Ignore | Out-Null
+
+Write-Host -ForegroundColor DarkGray "Installing AutopilotOOBE PS Module"
+Start-Process PowerShell -ArgumentList "-NoL -C Install-Module AutopilotOOBE -Force -Verbose" -Wait
+
+Write-Host -ForegroundColor DarkGray "Installing OSD PS Module"
+Start-Process PowerShell -ArgumentList "-NoL -C Install-Module OSD -Force -Verbose" -Wait
+
+Write-Host -ForegroundColor DarkGray "Executing Autopilot Check Script"
+Start-Process PowerShell -ArgumentList "-NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/GregoryB74/OSD-Cloud/main/Start-AutopilotPreCheck.ps1" -Wait
+
+Write-Host -ForegroundColor DarkGray "Executing Autopilot Check Script"
+Start-Process PowerShell -ArgumentList "-NoL -C Invoke-WebPSScript https://github.com/GregoryB74/OSD-Cloud/blob/main/Start-AutopilotRegistration.ps1" -Wait
+
+Write-Host -ForegroundColor DarkGray "Executing Cleanup Script"
+Start-Process PowerShell -ArgumentList "-NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/GregoryB74/OSD-Cloud/main/Start-CleanUp.ps1" -Wait
+
+# Cleanup scheduled Tasks
+Write-Host -ForegroundColor DarkGray "Unregistering Scheduled Tasks"
+Unregister-ScheduledTask -TaskName "Scheduled Task for SendKeys" -Confirm:`$false
+Unregister-ScheduledTask -TaskName "Scheduled Task for OSDCloud post installation" -Confirm:`$false
+
+Write-Host -ForegroundColor DarkGray "Restarting Computer"
+Start-Process PowerShell -ArgumentList "-NoL -C Restart-Computer -Force" -Wait
+
+Stop-Transcript -Verbose | Out-File
+"@
+
+Out-File -FilePath $ScriptPathOOBE -InputObject $OOBEScript -Encoding ascii
+
+$SendKeysScript = @"
+`$Global:Transcript = "`$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-SendKeys.log"
+Start-Transcript -Path (Join-Path "`$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" `$Global:Transcript) -ErrorAction Ignore | Out-Null
+
+Write-Host -ForegroundColor DarkGray "Stop Debug-Mode (SHIFT + F10) with WscriptShell.SendKeys"
+`$WscriptShell = New-Object -com Wscript.Shell
+
+start-sleep -s 5
+
+# Shift + F10
+Write-Host -ForegroundColor DarkGray "SendKeys: SHIFT + F10"
+`$WscriptShell.SendKeys("+({F10})")
+
+start-sleep -s 5
+
+# ALT + TAB
+Write-Host -ForegroundColor DarkGray "SendKeys: ALT + TAB"
+`$WscriptShell.SendKeys("%({TAB})")
+
+Start-Sleep -Seconds 1
+
+Stop-Transcript -Verbose | Out-File
+"@
+
+Out-File -FilePath $ScriptPathSendKeys -InputObject $SendKeysScript -Encoding ascii
+
+# Download ServiceUI.exe
+Write-Host -ForegroundColor Gray "Download Tools from GitHub Repo"
+Invoke-WebRequest https://github.com/GregoryB74/Tools/raw/main/ServiceUI64.exe -OutFile "C:\OSDCloud\ServiceUI.exe"
+Invoke-WebRequest https://github.com/GregoryB74/Tools/raw/main/CMTrace.exe -OutFile "C:\OSDCloud\CMTrace.exe"
+Invoke-WebRequest https://github.com/GregoryB74/Tools/raw/main/CMTrace.exe -OutFile "C:\Windows\System32\CMTrace.exe"
+
+#Create Scheduled Task for SendKeys with 15 seconds delay
+$TaskName = "Scheduled Task for SendKeys"
+
+$ShedService = New-Object -comobject 'Schedule.Service'
+$ShedService.Connect()
+
+$Task = $ShedService.NewTask(0)
+$Task.RegistrationInfo.Description = $taskName
+$Task.Settings.Enabled = $true
+$Task.Settings.AllowDemandStart = $true
+
+# https://msdn.microsoft.com/en-us/library/windows/desktop/aa383987(v=vs.85).aspx
+$trigger = $task.triggers.Create(9) # 0 EventTrigger, 1 TimeTrigger, 2 DailyTrigger, 3 WeeklyTrigger, 4 MonthlyTrigger, 5 MonthlyDOWTrigger, 6 IdleTrigger, 7 RegistrationTrigger, 8 BootTrigger, 9 LogonTrigger
+$trigger.Delay = 'PT15S'
+$trigger.Enabled = $true
+
+$action = $Task.Actions.Create(0)
+$action.Path = 'C:\OSDCloud\ServiceUI.exe'
+$action.Arguments = '-process:RuntimeBroker.exe C:\WINDOWS\System32\WindowsPowerShell\v1.0\powershell.exe ' + $ScriptPathSendKeys + ' -NoExit'
+
+$taskFolder = $ShedService.GetFolder("\")
+# https://msdn.microsoft.com/en-us/library/windows/desktop/aa382577(v=vs.85).aspx
+$taskFolder.RegisterTaskDefinition($TaskName, $Task , 6, "SYSTEM", $NULL, 5)
+
+# Create Scheduled Task for OSDCloud post installation with 20 seconds delay
+$TaskName = "Scheduled Task for OSDCloud post installation"
+
+$ShedService = New-Object -comobject 'Schedule.Service'
+$ShedService.Connect()
+
+$Task = $ShedService.NewTask(0)
+$Task.RegistrationInfo.Description = $taskName
+$Task.Settings.Enabled = $true
+$Task.Settings.AllowDemandStart = $true
+
+# https://msdn.microsoft.com/en-us/library/windows/desktop/aa383987(v=vs.85).aspx
+$trigger = $task.triggers.Create(9) # 0 EventTrigger, 1 TimeTrigger, 2 DailyTrigger, 3 WeeklyTrigger, 4 MonthlyTrigger, 5 MonthlyDOWTrigger, 6 IdleTrigger, 7 RegistrationTrigger, 8 BootTrigger, 9 LogonTrigger
+$trigger.Delay = 'PT20S'
+$trigger.Enabled = $true
+
+$action = $Task.Actions.Create(0)
+$action.Path = 'C:\OSDCloud\ServiceUI.exe'
+$action.Arguments = '-process:RuntimeBroker.exe C:\WINDOWS\System32\WindowsPowerShell\v1.0\powershell.exe ' + $ScriptPathOOBE + ' -NoExit'
+
+$taskFolder = $ShedService.GetFolder("\")
+# https://msdn.microsoft.com/en-us/library/windows/desktop/aa382577(v=vs.85).aspx
+$taskFolder.RegisterTaskDefinition($TaskName, $Task , 6, "SYSTEM", $NULL, 5)
